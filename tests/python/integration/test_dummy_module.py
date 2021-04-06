e0a41455ba3ca6aa9f96d0258c03d9faf17135f9
Alex Combessie <alex.combessie@gmail.com>
2021-04-06 12:46:03 +0200

    Dummy integration test

--- tests/python/integration/test_dummy_module.py
+++ tests/python/integration/test_dummy_module.py
@@ -1,11 +1,10 @@
+
 # -*- coding: utf-8 -*-
-# This is a test file intended to be used with pytest
-# pytest automatically runs all the function starting with "test_"
-# see https://docs.pytest.org for more information
-
-from dummy_module import dummy_function
+from dku_plugin_test_utils import dss_scenario
 
 
-def test_dummy_function():
-    dummy_results = dummy_function()
-    assert dummy_results == "foo"
+TEST_PROJECT_KEY = "TESTPLUGINTEMPLATE"
+
+
+def test_dummy_scenario(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="DUMMYTESTSCENARIO")
